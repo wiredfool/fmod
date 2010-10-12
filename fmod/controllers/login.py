9032068d537cf965ef52134575fcf7c0ccf968d7
erics <erics@mail2.wiredfool.com>
2010-10-12 06:30:30 +0000

    cleanup of obsolete/unnecessary stuff

--- fmod/controllers/login.py
+++ fmod/controllers/login.py
@@ -10,45 +10,6 @@ log = logging.getLogger(__name__)
 import md5
 
 class LoginController(BaseController):
-	# entirely from http://wiki.pylonshq.com/display/pylonscookbook/Simple+Homegrown+Authentication
-	def index(self):
-		"""
-		Show login form. Submits to /login/submit
-		"""
-		c.msg = ''
-		return render('login.mako')
-    
-	def submit(self):
-		"""
-		Verify username and password
-		"""
-		# Both fields filled?
-		form_username = str(request.params.get('username'))
-		form_password = str(request.params.get('password'))
-       
-		# Get user data from database
-		db_user = model.User().get_byName(form_username)
-		if db_user is None: # User does not exist
-			c.msg = "Sorry, that user is not known"
-			return render('login.mako')
-
-		# Wrong password? (MD5 hashes used here)
-		if not db_user.check_password(form_password):
-			c.msg = "Sorry, that password is incorrect"
-			return render('login.mako')
-
-		# Mark user as logged in
-		session['user'] = form_username
-		session.save()
-
-		# Send user back to the page he originally wanted to get to
-		if session.get('path_before_login'):
-			path = session['path_before_login']
-			del(session['path_before_login'])
-			redirect_to(path)
-		else: # if previous target is unknown just send the user to a welcome page
-			redirect_to(controller='profile', action='index')
-			
 
 	def logout(self):
 		"""
